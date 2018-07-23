80574f17387f575c60e784cbc30024bee69cb14f
nivred <emaildervin@gmail.com>
2018-07-23 14:10:35 -0500

    added routes folder with items.js containing GET, POST, and DELETE methods

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+const items = require('./routes/api/items');
+
 const app = express();
 
 // BodyParser Middleware
@@ -16,6 +18,9 @@ mongoose
     .then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log(err));
 
-    const port = process.env.PORT || 5000;
+// Use Routes
+app.use('/api/items', items);
 
-    app.listen(port, () => console.log(`Server started on port ${port}`))
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
